e439aa70e3dca9de60b5a084ac0895cb42229f49
James Ward <james@jamesward.org>
2016-09-07 09:04:26 -0600

    correctly serialize and deserialize the model

--- src/main/scala/ALSAlgorithm.scala
+++ src/main/scala/ALSAlgorithm.scala
@@ -1,19 +1,57 @@
-import io.prediction.controller.{P2LAlgorithm, Params}
-import io.prediction.data.storage.BiMap
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+
+import io.prediction.controller.{P2LAlgorithm, Params, PersistentModel, PersistentModelLoader}
+import io.prediction.data.storage.jdbc.JDBCModels
+import io.prediction.data.storage.{BiMap, Model, Storage}
+import io.prediction.workflow.KryoInstantiator
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+import org.apache.spark.rdd.RDD
 
 case class ALSAlgorithmParams(rank: Int, numIterations: Int, lambda: Double) extends Params
 
-class Model(
-             val model: MatrixFactorizationModel,
-             val userStringIntMap: BiMap[String, Int],
-             val itemStringIntMap: BiMap[String, Int]
-           ) extends Serializable
+case class ALSModel(
+             rank: Int,
+             userFeatures: Array[(Int, Array[Double])],
+             productFeatures: Array[(Int, Array[Double])],
+             userStringIntMap: BiMap[String, Int],
+             itemStringIntMap: BiMap[String, Int],
+             @transient maybeMatrixFactorizationModel: Option[MatrixFactorizationModel]
+           ) extends PersistentModel[ALSAlgorithmParams] {
+  override def save(id: String, params: ALSAlgorithmParams, sc: SparkContext): Boolean = {
+    val kryo = KryoInstantiator.newKryoInjection
+    val bytes = kryo(this)
+    ALSModel.jdbcModels.insert(Model(id, bytes))
+    true
+  }
+}
 
-class ALSAlgorithm(val params: ALSAlgorithmParams) extends P2LAlgorithm[PreparedData, Model, Query, PredictedResult] {
+object ALSModel extends PersistentModelLoader[ALSAlgorithmParams, ALSModel] {
 
-  def train(sc: SparkContext, data: PreparedData): Model = {
+  def jdbcModels = {
+    val config = Storage.getConfig("PGSQL").get
+    new JDBCModels(config.properties("URL"), config, "pio_model")
+  }
+
+  override def apply(id: String, params: ALSAlgorithmParams, maybeSparkContext: Option[SparkContext]): ALSModel = {
+    val model = jdbcModels.get(id).get
+
+    val kryo = KryoInstantiator.newKryoInjection
+
+    val alsModel = kryo.invert(model.models).get.asInstanceOf[ALSModel]
+
+    val userFeaturesRDD = maybeSparkContext.get.parallelize(alsModel.userFeatures)
+    val productFeaturesRDD = maybeSparkContext.get.parallelize(alsModel.productFeatures)
+
+    val matrixFactorizationModel = new MatrixFactorizationModel(alsModel.rank, userFeaturesRDD, productFeaturesRDD)
+
+    alsModel.copy(maybeMatrixFactorizationModel = Some(matrixFactorizationModel))
+  }
+}
+
+class ALSAlgorithm(val params: ALSAlgorithmParams) extends P2LAlgorithm[PreparedData, ALSModel, Query, PredictedResult] {
+
+  def train(sc: SparkContext, data: PreparedData): ALSModel = {
 
     val userStringIntMap = BiMap.stringInt(data.favorites.map(_.userId))
     val itemStringIntMap = BiMap.stringInt(data.favorites.map(_.propertyId))
@@ -24,13 +62,14 @@ class ALSAlgorithm(val params: ALSAlgorithmParams) extends P2LAlgorithm[Prepared
 
     val model = ALS.train(ratings, params.rank, params.numIterations, params.lambda)
 
-    new Model(model, userStringIntMap, itemStringIntMap)
+    new ALSModel(model.rank, model.userFeatures.collect(), model.productFeatures.collect(), userStringIntMap, itemStringIntMap, None)
   }
 
-  def predict(model: Model, query: Query): PredictedResult = {
+  def predict(model: ALSModel, query: Query): PredictedResult = {
+
     val userIdInt = model.userStringIntMap(query.userId)
 
-    val ratings = model.model.recommendProducts(userIdInt, query.numResults)
+    val ratings = model.maybeMatrixFactorizationModel.get.recommendProducts(userIdInt, query.numResults)
 
     val propertyRatings = ratings.map { rating =>
       model.itemStringIntMap.inverse(rating.product) -> rating.rating
